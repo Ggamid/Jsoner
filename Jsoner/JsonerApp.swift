f550158593acff0863b2b1830db83aa2bf968e19
Гамид Халидов <khalidov-gamid@mail.ru>
2024-06-04 21:20:00 +0300

    Initial Commit

---
+++ Jsoner/JsonerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonerApp.swift
+//  Jsoner
+//
+//  Created by Gamıd Khalıdov on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct JsonerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
